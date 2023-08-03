b7aa8aeec68439d992d5c5cffb43d6b357f39e56
Joshua Parkin <joshuadparkin@gmail.com>
2023-08-03 15:31:20 +0100

    add runtime to main file

--- src/bin/scryer-prolog.rs
+++ src/bin/scryer-prolog.rs
@@ -1,12 +1,19 @@
 fn main() {
-    use std::sync::atomic::Ordering;
-    use scryer_prolog::*;
     use scryer_prolog::atom_table::Atom;
+    use scryer_prolog::*;
+    use std::sync::atomic::Ordering;
 
     ctrlc::set_handler(move || {
         scryer_prolog::machine::INTERRUPT.store(true, Ordering::Relaxed);
-    }).unwrap();
+    })
+    .unwrap();
 
-    let mut wam = machine::Machine::new(Default::default());
-    wam.run_top_level(atom!("$toplevel"), (atom!("$repl"), 1));
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .build()
+        .expect("Failed to create Tokio runtime");
+    let _guard = rt.enter();
+    rt.block_on(async move {
+        let mut wam = machine::Machine::new(Default::default());
+        wam.run_top_level(atom!("$toplevel"), (atom!("$repl"), 1));
+    });
 }
